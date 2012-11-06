d70426b2aa8bb774c7395b781f8037d129dc5d81
ankithbti <ankithbti007@gmail.com>
2012-11-06 15:12:23 +0530

    First

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,53 @@
+class StudentsController < ApplicationController
+
+
+  def index
+    @students = Student.all
+    uri = URI.parse("https://localhost//api/v1/accounts?access_token=GtEXBYtduzmZbhrB6SHTSEqWZ3GhpORGIsArBzh6MyuGtJeVYBo4FcjX1BAemzpV")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    #raise response.body
+    @result = JSON.parse(response.body)
+    
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(params[:student])
+    if @student.save
+      redirect_to @student, :notice => "Successfully created student."
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update_attributes(params[:student])
+      redirect_to @student, :notice  => "Successfully updated student."
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to students_url, :notice => "Successfully destroyed student."
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CanvasApi::Application.config.secret_token = '47a41dc74afe2a0e889ef302b6592204a0205626088bd0b2f11987418de4e8011b467eb01cb8377ab7245fc88582e3ce996d189a730a44c50a281a14694045fd'
